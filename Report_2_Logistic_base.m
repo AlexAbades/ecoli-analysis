3d5b2379f8350d0e29b9f5878079c73fc7c15343
Alex <abadesgrimes31@gmail.com>
2021-11-11 17:36:57 +0100

    More changes

--- Report_2_Logistic_base.m
+++ Report_2_Logistic_base.m
@@ -5,6 +5,45 @@ load Ecoli_values.mat;
 % Drop the useless attributes 
 X(:,{'prot_name'}) = [];
 
+% Get the attribtes names
+attributeNames = [{'Offset'} X.Properties.VariableNames];
+
+% Split our data into the attributes and target 
+% Select our target 
+y = X(:,'cat'); 
+% Delete target from dataset 
+X(:,'cat') = [];
+
+% Convert tables into matrices 
+
+X = table2array(X);
+
+% Binarize the attributes that have only two values 
+X = TransformDataset(X);
+
+% Trnsform the categories into numbers 
+classLabels = table2cell(y);
+% Fiter the uniques values
+[classNames, ia, ic] = unique(classLabels);
+
+[~,y] = ismember(classLabels, classNames);
+% Substract 1 to the vector so it's starts from 0.
+y = y-1;
+
+k = ceil(0.6*size(X,1));
+CV = cvpartition(size(X,1),'Holdout', 0.3); % Specify a partition on 30%
+idx = CV.test;
+
+% Separate to training and test data
+X_train = X(~idx,:);
+y_train = y(~idx,:);
+X_test = X(idx,:);
+y_test = y(idx,:);
+
+N_test = size(X_test,1);
+N_train = size(X_train,1);
+
+
 %% Fit multinomial regression model
 Y_train=oneoutofk(y_train,C);
 Y_test=oneoutofk(y_test,C);
